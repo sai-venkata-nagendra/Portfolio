934922640d0f7fd18bcbe735f5e2c4948cdc4b17
sai-venkata-nagendra <153897632+sai-venkata-nagendra@users.noreply.github.com>
2025-07-11 06:40:06 +0530

    Delete MONEY/my-app directory

--- MONEY/my-app/src/components/Spacer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Spacer = () => {
-  // This component adds a large block of vertical space for future content.
-  return <div className="bg-[#00070E]" style={{ height: '3000px' }} />;
-};
-
-export default Spacer; 
